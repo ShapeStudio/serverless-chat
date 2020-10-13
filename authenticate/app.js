76308cc349b9b4909b1e182b03256fb61cffff3e
Marko Balazic <marko.balazic@gmail.com>
2020-10-13 20:30:00 +0200

    shape chat

--- authenticate/app.js
+++ authenticate/app.js
@@ -1,21 +1,31 @@
-// Copyright 2018-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2018-2020Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
 const AWS = require('aws-sdk');
 
 const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
 
+const { TABLE_NAME,  ROOMS } = process.env;
+
 exports.handler = async event => {
 
   console.log('CONNECTED: ', event.body)
+
+  /*
+
+  const postData = JSON.parse(event.body).data;
+
+  const userId = postData.userId;
 
   const putParams = {
     TableName: process.env.TABLE_NAME,
     Item: {
       connectionId: event.requestContext.connectionId,
-      timeStamp: Date.now()
+      timeStamp: Date.now(),
+      userId
     }
   };
+  
 
   try {
     await ddb.put(putParams).promise();
@@ -23,6 +33,7 @@ exports.handler = async event => {
     console.log("errr", err)
     return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
   }
+  */
 
-  return { statusCode: 200, body: 'Connected.' };
+  return { statusCode: 200, body: 'Authenticated.' };
 };
--- sendmessage/app.js
+++ sendmessage/app.js
@@ -5,15 +5,63 @@ const AWS = require('aws-sdk');
 
 const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
 
-const { TABLE_NAME } = process.env;
+const { TABLE_NAME,  ROOMS } = process.env;
 
 exports.handler = async event => {
+
+  console.log(process.env)
+
   let connectionData;
+  let rooms;
   
+  const postData = JSON.parse(event.body).data;
+  console.log(postData)
+
+  // const message = JSON.parse(postData).message
+  // const roomId = JSON.parse(postData).roomId
+  // console.log(message, roomId)
+  
+  const message = postData.message
+  const roomId = postData.roomId
+  console.log(message, roomId)
+
   try {
+
     connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
+    rooms = await ddb.scan({ TableName: ROOMS, FilterExpression : 'roomId = :roomId', ExpressionAttributeValues: {':roomId' : roomId} }).promise();
+    console.log("rooms:", rooms);
+
   } catch (e) {
+    console.log("error:", e)
     return { statusCode: 500, body: e.stack };
+  }
+
+  let room = rooms.Items[0]
+  console.log(room)
+
+  if(!room){
+    room = {
+      users:["1"],
+      messages:["hello"],
+      roomId: "2"
+    } 
+  }
+  console.log(room)
+
+  room.messages.push(message)
+
+  const putParams = {
+    TableName: ROOMS,
+    Item: room
+  };
+  console.log('saving data:', putParams);
+
+  try {
+    await ddb.put(putParams).promise();
+    console.log("saved")
+  } catch (err) {
+    console.log("errr", err)
+    return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
   }
   
   const apigwManagementApi = new AWS.ApiGatewayManagementApi({
@@ -21,11 +69,9 @@ exports.handler = async event => {
     endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
   });
   
-  const postData = JSON.parse(event.body).data;
-  
   const postCalls = connectionData.Items.map(async ({ connectionId }) => {
     try {
-      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
+      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: JSON.stringify(postData) }).promise();
     } catch (e) {
       if (e.statusCode === 410) {
         console.log(`Found stale connection, deleting ${connectionId}`);
@@ -39,6 +85,7 @@ exports.handler = async event => {
   try {
     await Promise.all(postCalls);
   } catch (e) {
+    console.log("error:", e)
     return { statusCode: 500, body: e.stack };
   }
 
